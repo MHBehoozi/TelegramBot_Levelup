0c23f975c2f4ca7fc22fede8e853e82a98a286c9
MHBehoozi <130773067+MHBehoozi@users.noreply.github.com>
2023-06-14 15:11:58 +0330

    Add math quiz and finalizing the bot

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from telegram import Update, ReplyKeyboardRemove, ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters, ConversationHandler
+from telegram import Update, ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters, ConversationHandler, CallbackQueryHandler
 import logging
+import random
+from typing import List
 from currency_converter import convert_currency_price_to_irr
 import constants
+
 
 logging.basicConfig(format='%(levelname)s - (%(asctime)s) - %(message)s - (Line: %(lineno)d) - [%(filename)s]',
                     filename='log.txt',
@@ -12,8 +15,6 @@ logging.basicConfig(format='%(levelname)s - (%(asctime)s) - %(message)s - (Line:
 
 logger = logging.getLogger(__name__)
 
-
-
 async def start_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logger.warning("user %s has stated the bot", update.effective_user.id)
     reply_keyboard=[
@@ -22,7 +23,10 @@ async def start_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 ]
     await context.bot.send_message(
         chat_id = update.message.chat.id,
-        text= "Welcome to my Telegram Bot.\nYou can see USD/IRR exchange ratio and also play two games including Dice and math quiz.",
+        text= "Welcome to my Telegram Bot.\n"
+        "You can see USD/IRR exchange ratio and also play two games including Dice and math quiz."
+        "\n\n\nNote: This test bot has been deployed on PythonAnywere.com and unfortunately on this free server this bot can't get price from API!!!"
+        "\nPlease deploy this bot localy to test exchange price notifier feature",
         reply_to_message_id= update.effective_message.id,
         reply_markup=ReplyKeyboardMarkup(
             keyboard=reply_keyboard,
@@ -39,7 +43,108 @@ async def dice_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
         reply_to_message_id= update.effective_message.id)
 
 # Math quiz
+START_ROUTES, END_ROUTES = range(2) ## Stages
+FIRST_QUESTION, SECOND_QUESTION, THIRD_QUESTION = range(3) ## Callback data
 
+def generate_question_and_return_answer(context: ContextTypes.DEFAULT_TYPE):
+    context.user_data['rand_x'] = random.randint(0, 100)
+    context.user_data['rand_y'] = random.randint(0, 100)
+    return context.user_data['rand_x'] + context.user_data['rand_y']
+
+def generate_user_keyboard(answer: int, state: int) -> List[List[InlineKeyboardButton]]:
+    keyboard = [
+        [
+            InlineKeyboardButton(text=f"{answer - 2}",
+                                 callback_data=f"{str(state)}_incorrect"),
+            InlineKeyboardButton(text=f"{answer + 1}",
+                                 callback_data=f"{str(state)}_incorrect")
+        ],
+        [
+            InlineKeyboardButton(text=f"{answer}",
+                                 callback_data=f"{str(state)}_correct"),
+            InlineKeyboardButton(text=f"{answer + 10}",
+                                 callback_data=f"{str(state)}_incorrect")
+        ]
+    ]
+    random.shuffle(keyboard)
+    return keyboard
+
+async def quiz_starter_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logger.warning("user %s started quiz conversation", update.effective_user.id)
+    answer = generate_question_and_return_answer(context)
+    keyboard = generate_user_keyboard(answer, FIRST_QUESTION)
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Let's start with a math pop quiz.\n\n"
+             f"1/3: What is {context.user_data['rand_x']} + {context.user_data['rand_y']} ?",
+        reply_markup=InlineKeyboardMarkup(inline_keyboard=keyboard)
+    )
+    return START_ROUTES
+
+async def first_question_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logger.warning("user %s entered first question", update.effective_user.id)
+    query = update.callback_query
+    await query.answer()
+    answer = generate_question_and_return_answer(context)
+    keyboard = generate_user_keyboard(answer, SECOND_QUESTION)
+    await query.edit_message_text(
+        text=f"Your answer was {query.data.split('_')[1]}.\n"
+             "Your question is:\n\n"
+             f"2/3: What's {context.user_data['rand_x']} + {context.user_data['rand_y']} ?",
+        reply_markup=InlineKeyboardMarkup(inline_keyboard=keyboard)
+    )
+    return START_ROUTES
+
+async def second_question_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logger.warning("user %s entered second question", update.effective_user.id)
+    query = update.callback_query
+    await query.answer()
+    answer = generate_question_and_return_answer(context)
+    keyboard = generate_user_keyboard(answer, THIRD_QUESTION)
+    await query.edit_message_text(
+        text=f"Your answer was {query.data.split('_')[1]}.\n"
+             "Your question is:\n\n"
+             f"3/3: What's {context.user_data['rand_x']} + {context.user_data['rand_y']} ?",
+        reply_markup=InlineKeyboardMarkup(inline_keyboard=keyboard)
+    )
+    return START_ROUTES
+
+async def third_question_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logger.warning("user %s entered third question", update.effective_user.id)
+    query = update.callback_query
+    await query.answer()
+    text = f"Your answer was {query.data.split('_')[1]}." \
+           f"Do you want to start over?"
+    keyboard = [
+        [InlineKeyboardButton(text="YES", callback_data="START_OVER")],
+        [InlineKeyboardButton(text="NO!", callback_data="CANCEL")]
+    ]
+    await query.edit_message_text(
+        text=text,
+        reply_markup=InlineKeyboardMarkup(inline_keyboard=keyboard),
+    )
+    return END_ROUTES
+
+async def start_over_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logger.warning("user %s start over quiz math", update.effective_user.id)
+    query = update.callback_query
+    await query.answer()
+    answer = generate_question_and_return_answer(context)
+    keyboard = generate_user_keyboard(answer, FIRST_QUESTION)
+    await query.edit_message_text(
+        text="Nice. Glad to have you.\n"
+             "Let's start.\n\n"
+             f"1/3: What is {context.user_data['rand_x']} + {context.user_data['rand_y']} ?",
+        reply_markup=InlineKeyboardMarkup(inline_keyboard=keyboard)
+    )
+    return START_ROUTES
+
+async def cancel_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logger.warning("user %s has ended the quiz conversation", update.effective_user.id)
+    query = update.callback_query
+    await query.answer()
+    await query.edit_message_text(text="Thank you for testing my bot!")
+    return ConversationHandler.END
 
 # Exchange section
 async def exchange_help_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -133,7 +238,6 @@ async def unset_alert_handler(update: Update, context: ContextTypes.DEFAULT_TYPE
         )
         logger.warning("user %s couldn't unset the exchange notifier job", update.effective_user.id)
 
-
 # Message Handlers
 def generate_response(text: str) -> str:
     parsed_text = text.lower().strip()
@@ -154,13 +258,6 @@ async def response_to_message(update: Update, context: ContextTypes.DEFAULT_TYPE
         return
     if "Play Dice" in update.effective_message.text:
         await dice_handler(update, context)
-        return
-    if "Play math quiz" in update.effective_message.text:
-        await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="This part is under construction",
-        reply_to_message_id=update.effective_message.id
-        )
         return
     if "IRR exchange ratio" in update.effective_message.text:
         await exchange_help_handler(update, context)
@@ -184,7 +281,7 @@ async def help_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logger.warning("user %s wants help", update.effective_user.id)
     await context.bot.send_message(
         chat_id= update.message.chat_id,
-        text="You shuold use buttons, Menu or following commands:\n /start \n /aboutme \n /dice",
+        text="You shuold use buttons, Menu or following commands:\n /start \n /aboutme \n or use provided markup keyborad.",
         reply_to_message_id= update.effective_message.id)
     
 async def aboutme_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -221,6 +318,24 @@ if __name__ == '__main__':
     bot.add_handler(CommandHandler("exchange", exchange_help_handler))
     bot.add_handler(CommandHandler("set", set_currency_alert_handler))
     bot.add_handler(CommandHandler("unset", unset_alert_handler))
+    conv_handler = ConversationHandler(
+            entry_points=[CommandHandler("mathquiz", quiz_starter_handler),
+                          MessageHandler(filters.Regex('Play math quiz'), quiz_starter_handler)],
+            states={
+                START_ROUTES: [
+                    CallbackQueryHandler(first_question_handler, pattern=f"^{str(FIRST_QUESTION)}_.*$"),
+                    CallbackQueryHandler(second_question_handler, pattern=f"^{str(SECOND_QUESTION)}_.*$"),
+                    CallbackQueryHandler(third_question_handler, pattern=f"^{str(THIRD_QUESTION)}_.*$")
+                ],
+                END_ROUTES: [
+                    CallbackQueryHandler(start_over_handler, pattern=f"^START_OVER$"),
+                    CallbackQueryHandler(cancel_handler, pattern=f"^CANCEL$")
+                ]
+            },
+            fallbacks=[],
+            allow_reentry=True,
+        )
+    bot.add_handler(conv_handler)
     bot.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), response_to_message))
     bot.add_error_handler(error_handler)
     bot.run_polling(allowed_updates= ['TEXT', 'COMMAND'])
