980ddcf01278c319a08f599605773dd8ea79828e
MHBehoozi <130773067+MHBehoozi@users.noreply.github.com>
2023-06-17 10:12:48 +0330

    Update constants.py

--- constants.py
+++ constants.py
@@ -2,6 +2,6 @@ from typing import Final
 
 TOKEN : Final = ""
 ExAPI : Final = ""
-ExURL : Final = ""
+ExURL : Final = "https://exapi.sm4rt.ir/api/v1"
 BotUsername : Final = "@levelup_mhbehroozi_bot"
 DevID : Final = [""]
